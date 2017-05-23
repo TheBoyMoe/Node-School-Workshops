ec133308580afd65aea60eced5b0911b75962cdc
bill fero <billfero@gmail.com>
2017-05-23 19:30:02 +0100

    impl node time server, returns current date and time

---
+++ learn-node-workshop/10-time-server.js
@@ -0,0 +1,15 @@
+'use strict';
+const net = require('net');
+
+const zeroFill = (val) => {
+    return (val < 10)? '0' + val: '' + val;
+};
+
+const listener = (socket) => {
+    let now = new Date();
+    let time = `${now.getFullYear()}-${zeroFill(now.getMonth() + 1)}-${zeroFill(now.getDate())} ${zeroFill(now.getHours())}:${zeroFill(now.getMinutes())}`;
+    socket.end(time + '\n');
+};
+
+let server = net.createServer(listener);
+server.listen(Number(process.argv[2])); // listen on supplied port
