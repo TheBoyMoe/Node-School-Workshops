e09ccb6369b347de4e32d1b255a7d66deec23a52
bill fero <billfero@gmail.com>
2017-06-01 18:20:47 +0100

    use reduce to count number of times particular words occur in a list

--- functional-js/05-some-and-every.js
+++ functional-js/05-some-and-every.js
@@ -1,5 +1,5 @@
 'use strict';
-// takes a list of valid users, and returns a function that returns true if all of the supplied users exist in the original list of users
+// takes a list of valid users, and returns a function that returns true if all of the submitted users exist in the original list of users (goodUsers)
 function checkUsersValid(goodUsers) {
 	
 	return function allUsersValid(submittedUsers) {
---
+++ functional-js/06-basic-reduce.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function countWords(inputWords) {
+	return inputWords.reduce((obj, word) => {
+		obj[word] = ++obj[word] || 1;
+		return obj;
+	}, {})
+}
+
+module.exports = countWords;
