bcb2217047473b92046a67f586f79821791746ae
bill fero <billfero@gmail.com>
2017-06-01 18:04:39 +0100

    using Array some and every to compare two arrays

---
+++ functional-js/05-some-and-every.js
@@ -0,0 +1,14 @@
+'use strict';
+// takes a list of valid users, and returns a function that returns true if all of the supplied users exist in the original list of users
+function checkUsersValid(goodUsers) {
+	
+	return function allUsersValid(submittedUsers) {
+		return submittedUsers.every((submittedUser) => { // returns true if all values match
+			return goodUsers.some((goodUser) => { // returns true if one or more matches found
+				return goodUser.id === submittedUser.id;
+			})
+		})
+	};
+}
+
+module.exports = checkUsersValid;
